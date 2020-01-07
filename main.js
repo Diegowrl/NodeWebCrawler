2897015bdccbd02828ec31688eec4a69a8bc5707
unknown <diegowrl@gmail.com>
2020-01-07 19:13:31 -0300

    Add the for loop on the beggin

--- main.js
+++ main.js
@@ -4,39 +4,76 @@ const fs = require('fs');
 
 var name = [];
 var cont = 50;
-var permiter = true;
 
-if(permiter){
-  request('https://myanimelist.net/topanime.php',(err,res,body)=>{
-      if (err) {console.log('Erro' + err)};
-
-      const $ = cheerio.load(body);
-      
-      $('.ranking-list').each(function(){ 
-        name +=  ',' + $(this).find('.di-ib a').html();
-        permiter = false;
-      });
-      //name = name.substr(1)
-      //fs.appendFileSync('myanimelist.txt', name + '\n');
-      
-  });
-
-}
-
-if(cont < 100) {
-  request(`https://myanimelist.net/topanime.php?limit=${cont}`,(err,res,body)=>{
-      if (err) {console.log('Erro' + err)};
-
-      const $ = cheerio.load(body);
-      
-      $('.ranking-list').each(function(){ 
-        name +=  ',' + $(this).find('.di-ib a').html();
-        permiter = false;
-      });
-
-      name = name.substr(1)
-      fs.appendFileSync('myanimelist.txt', name + '\n');
+for (let index = 0; index < 2; index++) {
+  console.log(name)
+  if(index == 0){
+    request('https://myanimelist.net/topanime.php',(err,res,body)=>{
+        if (err) {console.log('Erro' + err)};
+  
+        const $ = cheerio.load(body);
+        
+        $('.ranking-list').each(function(){ 
+          name +=  ',' + $(this).find('.di-ib a').html();
+        });
     });
+  
+  }else{
+    
+    cont = index * 50;
+    console.log(cont);
+
+    request(`https://myanimelist.net/topanime.php?limit=${cont}`,(err,res,body)=>{
+        if (err) {console.log('Erro' + err)};
+  
+        const $ = cheerio.load(body);
+        
+        $('.ranking-list').each(function(){ 
+          name +=  ',' + $(this).find('.di-ib a').html();
+          cont++;
+        });
+        name = name.substr(1);
+        fs.appendFileSync('myanimelist.txt', name + '\n');
+       
+      });
+  }
 }
 
-console.log(name);
+
+//console.log(name);
+
+
+// if(permiter){
+//   request('https://myanimelist.net/topanime.php',(err,res,body)=>{
+//       if (err) {console.log('Erro' + err)};
+
+//       const $ = cheerio.load(body);
+      
+//       $('.ranking-list').each(function(){ 
+//         name +=  ',' + $(this).find('.di-ib a').html();
+//         permiter = false;
+//       });
+//       //name = name.substr(1)
+//       //fs.appendFileSync('myanimelist.txt', name + '\n');
+      
+//   });
+
+// }
+
+// if(cont < 100) {
+//   request(`https://myanimelist.net/topanime.php?limit=${cont}`,(err,res,body)=>{
+//       if (err) {console.log('Erro' + err)};
+
+//       const $ = cheerio.load(body);
+      
+//       $('.ranking-list').each(function(){ 
+//         name +=  ',' + $(this).find('.di-ib a').html();
+//         permiter = false;
+//       });
+
+//       name = name.substr(1)
+//       fs.appendFileSync('myanimelist.txt', name + '\n');
+//     });
+// }
+
+// console.log(name);
