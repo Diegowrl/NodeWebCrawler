d7c9c8c613e1ca95eb4b47388cbf320cdc667678
unknown <diegowrl@gmail.com>
2020-01-07 21:51:53 -0300

    Add sequelize and mysql2

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 var name = [];
-var cont = 50;
+var cont;
 
 for (let index = 0; index < 2; index++) {
-  console.log(name)
+
   if(index == 0){
     request('https://myanimelist.net/topanime.php',(err,res,body)=>{
         if (err) {console.log('Erro' + err)};
   
         const $ = cheerio.load(body);
         
-        $('.ranking-list').each(function(){ 
-          name +=  ',' + $(this).find('.di-ib a').html();
+        $('.ranking-list').each(function(){
+          var data = $(this).find('.di-ib a').html();
+          var star = $(this).find('.di-ib span').html();
+          name.push('{ "Title": "' + data + '","Score":'+ star +'}');
         });
     });
   
   }else{
-    
     cont = index * 50;
-    console.log(cont);
 
     request(`https://myanimelist.net/topanime.php?limit=${cont}`,(err,res,body)=>{
         if (err) {console.log('Erro' + err)};
@@ -29,20 +29,30 @@ for (let index = 0; index < 2; index++) {
         const $ = cheerio.load(body);
         
         $('.ranking-list').each(function(){ 
-          name +=  ',' + $(this).find('.di-ib a').html();
-          cont++;
+          var data = $(this).find('.di-ib a').html();
+          var star = $(this).find('.di-ib span').html();
+          name.push('{ "Title": "' + data + '","Score":'+ star +'}');
         });
-        name = name.substr(1);
-        fs.appendFileSync('myanimelist.txt', name + '\n');
-       
+        
+        //obj = JSON.parse(name);
+        fs.appendFileSync('myanimelist.json', name);
+        
+        //console.log(obj)
       });
   }
 }
 
 
+
+
+
+
+
+
+
+
+
 //console.log(name);
-
-
 // if(permiter){
 //   request('https://myanimelist.net/topanime.php',(err,res,body)=>{
 //       if (err) {console.log('Erro' + err)};
